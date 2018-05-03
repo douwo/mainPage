6e53d5c076136e4b9a8c0d18df2b0efcf589d521
douwo <874952595@qq.com>
2018-05-03 14:09:27 +0800

    20180503
    添加锚点动画

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
+$(function(){
+	titleZeroText_P = $("#titlezero").offset().top;
+	titleOneText_P = $("#titleone").offset().top;
+	titleTwoText_P = $("#titletwo").offset().top;
+})
+
 $(".users-nav-title>li>a").on("click", function() {
 	$(".users-nav-title>li").removeClass("active");
 	$(this).parent("li").addClass("active");
@@ -9,4 +15,32 @@ $.get("text.json", function(result) {
 	$.each(result.newsContent, function(i) {
 		$("#newsContent" + (i + 1) + "_Text").html(result.newsContent[i]);
 	});
+})
+$("#goTop").click(function() {
+	$('body,html').animate({
+		scrollTop: 0
+	}, 800);
+})
+$(document).scroll(function() {
+	var top = $(document).scrollTop();
+	if(top < 250) {
+		$('#goTop').hide(300);
+	} else {
+		$('#goTop').show(300);
+	}
+})
+$("#zero").click(function(){
+	$('body,html').animate({
+		scrollTop: titleZeroText_P
+	}, 600);
+})
+$("#one").click(function(){
+	$('body,html').animate({
+		scrollTop: titleOneText_P
+	}, 600);
+})
+$("#two").click(function(){
+	$('body,html').animate({
+		scrollTop: titleTwoText_P
+	}, 600);
 })
