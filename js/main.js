d20511a74f570fafaaba9c0d488683d5b3742302
douwo <874952595@qq.com>
2018-05-03 09:25:11 +0800

    20180503
    添加部分动态效果，添加模态框。修改部分色调。

--- js/main.js
+++ js/main.js
@@ -1,5 +1,12 @@
-//var nav_li = $(".users-nav-title>li");
-$(".users-nav-title>li>a").on("click",function(){
+$(".users-nav-title>li>a").on("click", function() {
 	$(".users-nav-title>li").removeClass("active");
 	$(this).parent("li").addClass("active");
+})
+$.get("text.json", function(result) {
+	$.each(result.newsTitle, function(i) {
+		$("#news>li:eq(" + i + ")>a>span:first").html(result.newsTitle[i]);
+	});
+	$.each(result.newsContent, function(i) {
+		$("#newsContent" + (i + 1) + "_Text").html(result.newsContent[i]);
+	});
 })
